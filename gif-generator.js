32a21ae99b0112dc28d8df647a5c3f2682b8919e
Janine Lapiz <jglapiz@up.edu.ph>
2020-09-24 21:16:06 +0800

    Converted promises in gif-generator.js to async-await

--- gif-generator.js
+++ gif-generator.js
@@ -1,19 +1,15 @@
 const div = document.querySelector('div', {mode: 'cors'});
 
-fetch('https://api.giphy.com/v1/gifs/search?api_key=i3Pr1NOafNmcxCgKSFMef6dtsjkdlEFO&q=light yagami&limit=25&offset=0&rating=g&lang=en')
-    .then(response => response.json())
-    .then(response => {
-        console.log(response.data);
-        const data = response.data;
-        response.data.map(image => {
-            const gif = document.createElement('img');
-            gif.setAttribute('src', `https://media0.giphy.com/media/${image.id}/giphy.gif`);
-            gif.setAttribute('title', image.title);
-            div.appendChild(gif);
-
+    async function getGIFS() {
+        const response = await fetch('https://api.giphy.com/v1/gifs/search?api_key=i3Pr1NOafNmcxCgKSFMef6dtsjkdlEFO&q=light yagami&limit=25&offset=0&rating=g&lang=en', {mode: 'cors'});
+        const jsonGIF = await response.json();
+        const dataGIFS = await jsonGIF.data;
+        dataGIFS.map(gif => {
+            const image = document.createElement('img');
+            image.setAttribute('title', gif.title);
+            image.setAttribute('src', `https://media0.giphy.com/media/${gif.id}/giphy.gif`);
+            div.appendChild(image);
         });
-        console.log(Object.keys(data[3]), Object.values(data[3]));
-    });
+    }
 
-
-
+getGIFS();
